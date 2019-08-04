29be3ab5da2fd7485568cb03ff057c662c53728b
Ylli Abazi <ylli.abazi019@gmail.com>
2019-08-04 17:49:19 +0200

    Add color and black button.

--- index.js
+++ index.js
@@ -12,8 +12,26 @@ btnNewGrid.textContent = "New grid";
 btnNewGrid.className = "btn";
 interact.appendChild(btnNewGrid);
 
+let btnColorful = document.createElement('button');
+btnColorful.textContent = "Random Colors";
+btnColorful.className = "btn";
+interact.appendChild(btnColorful);
+
+let btnBlack = document.createElement('button');
+btnBlack.textContent = "Black";
+btnBlack.className = "btn";
+interact.appendChild(btnBlack);
+
 const grid = document.querySelectorAll(".div");
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function setUpGrid(number){
     for (let i = 0; i < number*number; i++){
@@ -24,10 +42,25 @@ function setUpGrid(number){
         container.appendChild(div);
     };
     const grid = document.querySelectorAll(".div");
+    
+    let bgColor = 0;
+    
+    btnColorful.addEventListener('click', function(e){
+        bgColor = 1;
+    });
+    
+    btnBlack.addEventListener('click', function(e){
+        bgColor = 0;
+    });
+    
     grid.forEach(function(e){
         e.addEventListener("mouseover", function(event){
-            event.target.style.background = "black";
-        })
+            if (bgColor == 1){
+                event.target.style.background = getRandomColor();
+            }else if (bgColor == 0){
+                event.target.style.background = 'black';
+            };
+        });
     });
     btnClear.addEventListener("click", function(event){
         grid.forEach(function(e){
@@ -40,7 +73,7 @@ setUpGrid(32);
 function clearGrid(){
     document.querySelectorAll(".div").forEach((grid) => {
         grid.parentNode.removeChild(grid);
-    })
+    });
 };
 
 btnNewGrid.addEventListener('click', function(){
